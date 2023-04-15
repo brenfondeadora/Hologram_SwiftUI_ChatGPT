96255219c2815cba8b99a607b6f00a7a9c491bc8
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-15 11:02:21 -0600

    Initial Commit

---
+++ Hologram_SwiftUI_ChatGPT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hologram_SwiftUI_ChatGPT
+//
+//  Created by Brenda Saavedra  on 15/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hologram_SwiftUI_ChatGPT/Hologram_SwiftUI_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hologram_SwiftUI_ChatGPTApp.swift
+//  Hologram_SwiftUI_ChatGPT
+//
+//  Created by Brenda Saavedra  on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Hologram_SwiftUI_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
