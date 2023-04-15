03fa0253efa3035ca8c5c1aae869094706224b20
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-15 12:54:20 -0600

    style: change galaxy pattern

--- Hologram_SwiftUI_ChatGPT/ContentView.swift
+++ Hologram_SwiftUI_ChatGPT/ContentView.swift
@@ -8,10 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isGalaxy = false
+    
     var body: some View {
-        SunflowerView()
-            .background(Color.black)
-            .edgesIgnoringSafeArea(.all)
+        ZStack {
+            SunflowerView()
+                .opacity(isGalaxy ? 0.0 : 1.0)
+                .animation(.easeInOut(duration: 1.5))
+            
+            GalaxyView()
+                .opacity(isGalaxy ? 1.0 : 0.0)
+                .animation(.easeInOut(duration: 1.5))
+        }
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+        .onTapGesture {
+            withAnimation(.spring()) {
+                isGalaxy.toggle()
+            }
+        }
     }
 }
 
@@ -23,14 +38,14 @@ struct ContentView_Previews: PreviewProvider {
 
 struct SunflowerShape: Shape {
     let seedCount: Int
-
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
-
+        
         let angleIncrement = 2 * Double.pi * (1.0 - (1.0 / Constants.goldenRatio))
         let maxRadius = min(rect.width, rect.height) * 0.5
         let center = CGPoint(x: rect.width * 0.5, y: rect.height * 0.5)
-
+        
         for i in 0..<seedCount {
             let radius = sqrt(Double(i)) * maxRadius / sqrt(Double(seedCount))
             let angle = Double(i) * angleIncrement
@@ -40,10 +55,10 @@ struct SunflowerShape: Shape {
             let circle = Path(ellipseIn: CGRect(x: x - circleSize * 0.5, y: y - circleSize * 0.5, width: circleSize, height: circleSize))
             path.addPath(circle)
         }
-
+        
         return path
     }
-
+    
     struct Constants {
         static let goldenRatio: Double = 1.61803398875
     }
@@ -51,7 +66,7 @@ struct SunflowerShape: Shape {
 
 struct SunflowerView: View {
     private let seedCount = 200
-
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -65,32 +80,67 @@ struct SunflowerView: View {
             .frame(width: geometry.size.width, height: geometry.size.height)
         }
     }
-
+    
     private func randomColor() -> Color {
         let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple]
         return colors.randomElement() ?? .black
     }
-
+    
     private func circlePosition(index: Int, rect: CGRect) -> CGPoint {
         let angleIncrement = 2 * Double.pi * (1.0 - (1.0 / Constants.goldenRatio))
         let maxRadius = min(rect.width, rect.height) * 0.5
         let center = CGPoint(x: rect.width * 0.5, y: rect.height * 0.5)
-
+        
         let radius = sqrt(Double(index)) * maxRadius / sqrt(Double(seedCount))
         let angle = Double(index) * angleIncrement
         let x = center.x + CGFloat(radius * cos(angle))
         let y = center.y + CGFloat(radius * sin(angle))
-
+        
         return CGPoint(x: x, y: y)
     }
-
+    
     private func circleSize(index: Int, rect: CGRect) -> CGFloat {
         let maxRadius = min(rect.width, rect.height) * 0.5
         let radius = sqrt(Double(index)) * maxRadius / sqrt(Double(seedCount))
         return CGFloat(1.0 - (radius / maxRadius)) * 8 * 3
     }
-
+    
     struct Constants {
         static let goldenRatio: Double = 1.61803398875
+    }
+}
+
+struct GalaxyView: View {
+    private let pointCount = 700
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0..<pointCount) { index in
+                    Circle()
+                        .fill(randomColor())
+                        .frame(width: 10, height: 10)
+                        .position(pointPosition(index: index, rect: geometry.frame(in: .local)))
+                }
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
+    }
+    
+    private func randomColor() -> Color {
+        let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple]
+        return colors.randomElement() ?? .black
+    }
+    
+    private func pointPosition(index: Int, rect: CGRect) -> CGPoint {
+        let angleIncrement = 2 * Double.pi / Double(pointCount)
+        let maxRadius = sqrt(pow(rect.width, 2) + pow(rect.height, 2)) / 2
+        let center = CGPoint(x: rect.width * 0.5, y: rect.height * 0.5)
+        let radius = Double.random(in: 0..<maxRadius)
+        let angle = Double(index) * angleIncrement
+        let x = center.x + CGFloat(radius * cos(angle))
+        let y = center.y + CGFloat(radius * sin(angle))
+        
+        return CGPoint(x: x, y: y)
     }
 }
